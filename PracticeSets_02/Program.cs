e90af1ab0b4133c6485a8c4113a1163313328348
Carl De Castro <carldecastro888@gmail.com>
2024-08-28 13:39:13 +0800

    Initial commit

--- PracticeSets_02/Program.cs
+++ PracticeSets_02/Program.cs
@@ -4,18 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PracticeSets_02
+namespace PS02_05
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's print the odd numbers");
-            int odd_number = 20;
-
-            for (int i = 1; i < odd_number; i += 2)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("Enter a character: ");
+                char letter = char.Parse(Console.ReadLine());
+
+
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    Console.WriteLine("is a vowel");
+                }
+                else if (letter == 'b' || letter == 'c' || letter == 'd' || letter == 'f' ||
+                    letter == 'g' || letter == 'h' || letter == 'j' || letter == 'k' || letter == 'l' || letter == 'm' ||
+                    letter == 'n' || letter == 'p' || letter == 'q' || letter == 'r' || letter == 's' || letter == 't' ||
+                    letter == 'v' || letter == 'w' || letter == 'x' || letter == 'y' || letter == 'z')
+                {
+                    Console.WriteLine("is a consonant");
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+
             }
         }
     }
