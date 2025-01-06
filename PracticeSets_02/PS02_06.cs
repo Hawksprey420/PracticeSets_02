f6c2da6a46043cde695534ccd306cc70a72fa566
Carl De Castro <carldecastro989@gmail.com>
2025-01-06 23:44:07 +0800

    Added PS02_06

---
+++ PracticeSets_02/PS02_06.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSets_02
+{
+    internal class PS02_06
+    {
+        public static void Palindrome ()
+        {
+            {
+                Console.WriteLine(" Enter a Word: ");
+                string Palindrome = Console.ReadLine();
+                string check = new string(Palindrome.Reverse().ToArray());
+
+                if (Palindrome.Contains(check))
+                {
+                    Console.WriteLine("The Word " + Palindrome + " is Palindrome ");
+                }
+                else if (Palindrome.Contains(check))
+                {
+                    Console.WriteLine("The Word" + Palindrome + " is not Palindrome ");
+                }
+                else
+                {
+                    Console.WriteLine("The Word" + Palindrome + " is Invalid ");
+                }
+            }
+        }
+    }
+}
